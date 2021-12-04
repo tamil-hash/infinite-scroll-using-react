4982fe8f03c11333fc559e2e476868a9a4e7c4e5
--unset <tamilselvan@kloudlearn.com>
2021-12-04 20:37:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(0);
+
+  const getUser = async () => {
+    console.log("ok");
+    setPage((page) => page + 1);
+    const url = `https://randomuser.me/api/?page=${page}&results=10`;
+    const response = await fetch(url);
+    const usersData = await response.json();
+    let allUsers = [];
+    allUsers = usersData.results.map((user) => {
+      return {
+        name: user.name.first + " " + user.name.last,
+        picture: user.picture.thumbnail,
+        email: user.email,
+        id: user.phone,
+      };
+    });
+    setUsers([...users, ...allUsers]);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="content">
+        <InfiniteScroll
+          className="users-container"
+          dataLength={users.length}
+          hasMore={true}
+          next={getUser}
+          height={500}
+          loader={<h4>Loading...</h4>}
+          scrollableTarget="scrollableDiv"
         >
-          Learn React
-        </a>
-      </header>
+          {users.map((user) => (
+            <div key={user.id} className="each-user">
+              <img className="user-img" src={user.picture} alt={user.name} />
+              <div className="info">
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+              </div>
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
